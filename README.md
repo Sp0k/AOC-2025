252b13b9409a65e80a8dda6a46b8834ff98e404d
Sp0k <contact@gabsavard.com>
2025-12-02 00:19:14 -0400

    complete day 1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Advent of Code 2025
+
+Here are my solutions for the 2025 Advent of Code Puzzles!
+
+## Time
+| Day | Part 1 | Part 2 |
+| :-: | :----: | :----: |
+| 1 | 21:54:17 | 22:41:17 |
+| 2 | N/A | N/A |
+| 3 | N/A | N/A |
+| 4 | N/A | N/A |
+| 5 | N/A | N/A |
+| 6 | N/A | N/A |
+| 7 | N/A | N/A |
+| 8 | N/A | N/A |
+| 9 | N/A | N/A |
+| 10 | N/A | N/A |
+| 11 | N/A | N/A |
+| 12 | N/A | N/A |
---
+++ days/day_1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Sp0k/AOC-2025/aoc"
+)
+
+var dialVal int
+var zeroCounterP1 int
+var zeroCounterP2 int
+
+func turnLeft(rotations int) {
+	if dialVal == 0 {
+		zeroCounterP2 += rotations / 100
+	} else if rotations >= dialVal {
+		zeroCounterP2 += 1 + (rotations - dialVal)/100
+	}
+	dialVal = ((dialVal - rotations)%100 + 100) % 100
+}
+
+func turnRight(rotations int) {
+	zeroCounterP2 += (dialVal + rotations) / 100
+	dialVal = (dialVal + rotations) % 100
+}
+
+func handleRotationInstruction(instruction string) {
+	rotation := aoc.MustAtoi(instruction[1:])
+
+	if rune(instruction[0]) == 'L' {
+		turnLeft(rotation)
+	} else {
+		turnRight(rotation)
+	}
+}
+
+func main() {
+	args := os.Args
+	if len(args) != 2 {
+		log.Fatal("Wrong number of arguments. Expected 2")
+	}
+
+	instructions := aoc.MustReadLines(args[1])
+	dialVal = 50
+	zeroCounterP1 = 0
+	zeroCounterP2 = 0
+
+	for i := range(instructions) {
+		handleRotationInstruction(instructions[i])
+		if (dialVal == 0) {
+			zeroCounterP1++
+		}
+	}
+
+	fmt.Println("First code: ", zeroCounterP1)
+	fmt.Println("Second code: ", zeroCounterP2)
+}
